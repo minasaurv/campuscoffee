4ef462f5f986e67fc9da8bea1ef04d397cbfee60
MinasaurV <itsmewillyv@gmail.com>
2026-03-09 09:45:30 -0500

    Update MainActivity

--- app/src/main/java/com/minasaur/campuscoffee/MainActivity.kt
+++ app/src/main/java/com/minasaur/campuscoffee/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.minasaur.campuscoffee
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onStart() {
@@ -53,6 +60,57 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btn = findViewById<Button>(R.id.btnSubmit)
+
+        val txtError = findViewById<TextView>(R.id.txtError)
+
+
+
+        btn.setOnClickListener { v ->
+
+            var valid = true
+            var errorMessage = ""
+
+            // Get name from EditText
+            val nameForOrder = findViewById<EditText>(R.id.nameForOrder).text
+
+            // Get selected type from radio
+            val coffeeTypeId = findViewById<RadioGroup>(R.id.coffeeType).checkedRadioButtonId
+            val coffeeType = findViewById<RadioButton>(coffeeTypeId)?.text
+
+            // Get selected size from radio
+            val coffeeSizeId = findViewById<RadioGroup>(R.id.coffeeSize).checkedRadioButtonId
+            val coffeeSize = findViewById<RadioButton>(coffeeSizeId)?.text
+
+            // Validate
+            if (nameForOrder.isEmpty()) {
+                errorMessage += "\nNo Name Submitted"
+                valid = false
+            }
+            if (coffeeType == null) {
+                errorMessage += "\nNo Coffee Type Selected"
+                valid = false
+            }
+            if (coffeeSize == null) {
+                errorMessage += "\nNo Size Selected"
+                valid = false
+            }
+
+            if (!valid) {
+                txtError.text = errorMessage
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this@MainActivity, ActivityTwo::class.java)
+
+            // Add Extras
+            intent.putExtra("Name", nameForOrder)
+            intent.putExtra("CoffeeType", coffeeType)
+            intent.putExtra("CoffeeSize", coffeeSize)
+
+            startActivity(intent)
         }
     }
 }
